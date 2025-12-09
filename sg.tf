4bb59a356331c1aa3e121e6ce2a1ed2a10e476c8
Aru1771 <kakiaravind1@gmail.com>
2025-12-09 14:28:15 +0530

    sg file

---
+++ sg.tf
@@ -0,0 +1,26 @@
+provider "aws_security_group" "sg-1" {
+name = "my-sg-2"
+description = "created by terraform"
+
+ingress {
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+from_port = 0
+to_port = 0
+protocol = -1
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+egress {
+from_port = 0
+to_port = 0
+protocol = -1
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+}
