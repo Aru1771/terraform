c4b940e513ee95b587a7e3c0447eaf8a3a6eff7f
root <root@ip-172-31-71-165.ec2.internal>
2025-12-09 10:48:45 +0000

    ec2 file modified

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ Name = var.name
 ami = var.ami_id
 instance_type = var.itype
 key_name = var.key 
-vpc_security_group_ids = var.sg
+vpc_security_group_ids = [var.sg]
 root_block_device {
 volume_size = var.size
 }
