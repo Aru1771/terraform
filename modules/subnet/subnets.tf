fa95a26feae8b1a7253f337faec57c92796a40e7
Aru1771 <kakiaravind1@gmail.com>
2026-06-09 21:41:12 +0530

    5th after adding the subnet2 file

--- modules/subnet/subnets.tf
+++ modules/subnet/subnets.tf
@@ -7,11 +7,3 @@ resource "aws_subnet" "subnet_1"{
     }
 }
 
-resource "aws_subnet" "subnet_2" {
-    vpc_id = var.vpc_id
-    cidr_block = var.cidr_block_2
-    availability_zone = var.availability_zone_2
-    tags = {
-        Name = var.subnet_name_2
-    }
-}
