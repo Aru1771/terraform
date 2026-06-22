4221ced1a5b0a0edc46a0548c97efb3a25d8c287
Aru1771 <kakiaravind1@gmail.com>
2026-06-22 16:41:51 +0530

    sg 3

--- modules/sg/variables.tf
+++ modules/sg/variables.tf
@@ -21,7 +21,7 @@ variable "ingress_rules" {
         from_port = number
         to_port = number
         protocol = string
-        cidr_block = list(string)
+        cidr_blocks = list(string)
     }))
     default = []
 
