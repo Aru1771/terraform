210d243b9ca98576376f8a997ae08ca319b54576
Aru1771 <kakiaravind1@gmail.com>
2026-08-06 20:05:54 +0530

    gp2 changed to gp3 in main.tf

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ module "ebs_volume" {
     source = "./modules/ebs"
     availability_zone = "us-east-1a"
     size = 8
-    type = "gp2"
+    type = "gp3"
     snapshot_id = ""
     iops = 3000
     throughput = 125
